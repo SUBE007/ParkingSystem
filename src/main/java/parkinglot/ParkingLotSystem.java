85cb49d34e74b7c64c5a658aaf2d6ac49557f230
SUBE007 <chaurasiyasube007@gmail.com>
2020-04-08 13:03:32 +0530

    modified use case2 commond query sepration

--- src/main/java/parkinglot/ParkingLotSystem.java
+++ src/main/java/parkinglot/ParkingLotSystem.java
@@ -1,20 +1,28 @@
 package parkinglot;
 
+import exception.ParkingLotException;
+
 public class ParkingLotSystem {
     private Object vehicle;
 
     public ParkingLotSystem(){ }
 
-    public boolean park(Object vehicle)   {
-        this.vehicle=vehicle;
-        return true;
-    }
+    public void park(Object vehicle) throws ParkingLotException {
+        if (this.vehicle!=null)
+             throw new ParkingLotException("Parking Lot is Full");
+          this.vehicle=vehicle;
+     }
     public boolean Unpark(Object vehicle) {
         if (vehicle==null) return false;
         if (this.vehicle.equals(vehicle)){
             this.vehicle=null;
             return true;
         }
+        return false;
+    }
+    public boolean isVehicleParked(Object vehicle) {
+        if (this.vehicle.equals(vehicle))
+            return true;
         return false;
     }
 }
