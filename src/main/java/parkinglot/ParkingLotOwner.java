387b730b18bca77c6316113e8e8e53ed2bb75ac6
SUBE007 <chaurasiyasube007@gmail.com>
2020-04-08 22:16:26 +0530

    as a parking lot owner i want to know when the lot is full so that he can put out full message

---
+++ src/main/java/parkinglot/ParkingLotOwner.java
@@ -0,0 +1,10 @@
+package parkinglot;
+
+public class ParkingLotOwner {
+    private boolean isFullCapacity;
+
+    public boolean capacityIsFull() {
+        isFullCapacity = true;
+        return isFullCapacity;
+    }
+}
--- src/main/java/parkinglot/ParkingLotSystem.java
+++ src/main/java/parkinglot/ParkingLotSystem.java
@@ -2,27 +2,54 @@ package parkinglot;
 
 import exception.ParkingLotException;
 
-public class ParkingLotSystem {
-    private Object vehicle;
+import java.util.ArrayList;
+import java.util.List;
 
-    public ParkingLotSystem(){ }
+
+public class ParkingLotSystem {
+    private int actualCapacity;
+    private List vehicles;
+    private ParkingLotOwner owner;
+
+    public ParkingLotSystem(int capacity){
+         this.vehicles=new ArrayList();
+         this.actualCapacity=capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.actualCapacity=capacity;
+    }
+
+    public void registerOwner(ParkingLotOwner owner) {
+        this.owner=owner;
+
+    }
 
     public void park(Object vehicle) throws ParkingLotException {
-        if (this.vehicle!=null)
-             throw new ParkingLotException("Parking Lot is Full");
-          this.vehicle=vehicle;
-     }
+        if (this.vehicles.size()==this.actualCapacity) {
+            owner.capacityIsFull();
+            throw new ParkingLotException("Parking Lot is Full");
+        }
+        if(isVehicleParked(vehicle))
+          throw new ParkingLotException(" Vehicle already Parked");
+        this.vehicles.add(vehicle);
+    }
+
     public boolean Unpark(Object vehicle) {
         if (vehicle==null) return false;
-        if (this.vehicle.equals(vehicle)){
-            this.vehicle=null;
+        if (this.vehicles.contains(vehicle)){
+            this.vehicles.remove(vehicle);
             return true;
         }
         return false;
     }
     public boolean isVehicleParked(Object vehicle) {
-        if (this.vehicle.equals(vehicle))
+        if (this.vehicles.contains(vehicle))
             return true;
         return false;
     }
+
+
+
+
 }
