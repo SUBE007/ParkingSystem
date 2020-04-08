dc456b2e1c24cde9eecc22d44b00b0abcc5cf711
SUBE007 <chaurasiyasube007@gmail.com>
2020-04-08 13:04:31 +0530

    modified use case2 commond query sepration

--- src/test/java/parkinglot/ParkingLotTest.java
+++ src/test/java/parkinglot/ParkingLotTest.java
@@ -1,21 +1,50 @@
 package parkinglot;
 
+import exception.ParkingLotException;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
+    ParkingLotSystem parkingLotSystem=null;
+    private Object vehicle;
+
+    @Before
+    public void setUp() throws Exception{
+        vehicle=new Object();
+        parkingLotSystem=new ParkingLotSystem();
+    }
+
     @Test
     public void givenVehicle_WhenParked_ShouldReturnTrue(){
-        ParkingLotSystem parkingLotSystem=new ParkingLotSystem();
-        boolean isParked=parkingLotSystem.park(new Object());
-        Assert.assertTrue(isParked);
+      try {
+          parkingLotSystem.park(vehicle);
+          boolean isParked = parkingLotSystem.isVehicleParked(vehicle);
+          Assert.assertTrue(isParked);
+      }catch (ParkingLotException e){
+            e.printStackTrace();
+        }
     }
+
     @Test
-    public void givenVehicle_WhenUnParked_ShouldReturnTrue(){
-        Object vehicle=new Object();
-        ParkingLotSystem parkingLotSystem=new ParkingLotSystem();
-        parkingLotSystem.park(vehicle);
-        boolean isUnParked=parkingLotSystem.Unpark(vehicle);
-        Assert.assertTrue(isUnParked);
+    public void givenAVehicle_WhenAlreadyParked_ShouldReturnFalse(){
+        try {
+            parkingLotSystem.park(vehicle);
+            parkingLotSystem.park(new Object());
+        }catch (ParkingLotException e){
+            Assert.assertEquals("Parking Lot is Full",e.getMessage());
+            e.printStackTrace();
+        }
+   }
+
+    @Test
+    public void givenAVehicle_WhenUnParked_ShouldReturnTrue(){
+        try {
+            parkingLotSystem.park(vehicle);
+            boolean isUnParked = parkingLotSystem.Unpark(vehicle);
+            Assert.assertTrue(isUnParked);
+        }catch (ParkingLotException e){
+            e.printStackTrace();
+        }
     }
 }
