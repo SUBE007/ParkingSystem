418d0a5c265cb9196a2d4f8ed9f22f1fa529e060
SUBE007 <chaurasiyasube007@gmail.com>
2020-04-08 23:29:16 +0530

    modified and added Observer Interface

--- src/main/java/parkinglot/ParkingLotSystem.java
+++ src/main/java/parkinglot/ParkingLotSystem.java
@@ -9,47 +9,45 @@ import java.util.List;
 public class ParkingLotSystem {
     private int actualCapacity;
     private List vehicles;
-   // private List <ParkingLotObserver> observers;
-    private ParkingLotOwner owner;
-    private AirportSecurity security;
+    private List<ParkingLotObserver> observers;
 
-    public ParkingLotSystem(int capacity){
-     //   this.observers=new ArrayList<>();
-         this.vehicles=new ArrayList();
-         this.actualCapacity=capacity;
+
+    public ParkingLotSystem(int capacity) {
+        this.observers = new ArrayList<>();
+        this.vehicles = new ArrayList();
+        this.actualCapacity = capacity;
     }
 
     public void setCapacity(int capacity) {
-        this.actualCapacity=capacity;
+        this.actualCapacity = capacity;
     }
 
-    public void registerOwner(ParkingLotOwner owner) {
-        this.owner=owner;
+    public void registerParkingLotObserver(ParkingLotObserver observer) {
+        this.observers.add(observer);
 
-    }
-    public void registerSecurity(AirportSecurity airportSecurity) {
-        this.security=airportSecurity;
     }
 
     public void park(Object vehicle) throws ParkingLotException {
-        if (this.vehicles.size()==this.actualCapacity) {
-            owner.isCapacityFull();
-            security.isCapacityFull();
+        if(this.vehicles.size()==this.actualCapacity) {
+            for (ParkingLotObserver observer : observers) {
+                observer.capacityIsFull();
+            }
             throw new ParkingLotException("Parking Lot is Full");
         }
         if(isVehicleParked(vehicle))
-          throw new ParkingLotException(" Vehicle already Parked");
+            throw new ParkingLotException(" Vehicle already Parked");
         this.vehicles.add(vehicle);
     }
 
     public boolean Unpark(Object vehicle) {
-        if (vehicle==null) return false;
-        if (this.vehicles.contains(vehicle)){
+        if (vehicle == null) return false;
+        if (this.vehicles.contains(vehicle)) {
             this.vehicles.remove(vehicle);
             return true;
         }
         return false;
     }
+
     public boolean isVehicleParked(Object vehicle) {
         if (this.vehicles.contains(vehicle))
             return true;
